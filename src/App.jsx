9e9d8bc65394e56d15c2053b34bfe77459297a19
Mikael <mikaelc623@gmail.com>
2025-09-17 17:29:17 -0400

    função de adicionar tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Tasks from "./components/tasks";
+import AddTask from "./components/AddTask";
 
 function App(){
     const [tasks, setTasks] = useState([{
@@ -42,8 +43,9 @@ function onDeleteTask(taskId){
 
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-        <div className="w-[500px]">
+        <div className="w-[500px] space-y-4">
         <h1 className="text-3xl m-8 text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
+        <AddTask /> 
         <Tasks tasks = {tasks} onTaskClick={onTaskClick} onDeleteTask={onDeleteTask}/>
         </div>
     </div>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,22 +1,22 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react"
 
-function Tasks(props){ 
+function Tasks({tasks, onTaskClick, onDeleteTask}){ 
     return (
-        <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-        {props.tasks.map((task) => (
+        <ul className="space-y-4 p-6 bg-slate-600 rounded-md shadow">
+        {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
             <button 
-            onClick={() => props.onTaskClick(task.id)} 
+            onClick={() => onTaskClick(task.id)} 
             className={`bg-slate-400 text-left w-full text-white p-2 rounded-md 
             ${task.isCompleted && 'line-through'
             }`}
             >
                 {task.title}
             </button>
-            <button className="bg-slate-400 p-2 rounded-md text-white">
+            <button className="bg-slate-400 p-2 rounded-md text-white transition-all duration-300 ease-in-out hover:scale-95">
                 <ChevronRightIcon />
             </button>
-            <button onClick={() => props.onDeleteTask(task.id)} className="bg-slate-400 p-2 rounded-md text-white">
+            <button onClick={() => onDeleteTask(task.id)} className="bg-slate-400 p-2 rounded-md text-white transition-all duration-300 ease-in-out hover:scale-95">
                 <TrashIcon />
             </button>
         </li>
